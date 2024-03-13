cef39590aff51d3d051b82b61e9fe91dad022c51
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2024-03-13 10:40:07 +0100

    (läste fel i instruktionerna) nu har jag fixat så att den kör båda metoderna.

--- src/App.java
+++ src/App.java
@@ -6,11 +6,18 @@ public class App {
         }
         System.out.println();
         skriv_ut_array(fält);
+        System.out.println();
+        antal_över_8(fält);
     }
     static void skriv_ut_array(int[] array){
         for (int i = 0; i < array.length; i++) {
-            if (array[i]>8) {
-                System.out.println(array[i]);
+            System.out.println(array[i]);
+        }
+    }
+    static void antal_över_8(int[]arrayen){
+        for (int j = 0; j < arrayen.length; j++) {
+            if (arrayen[j]>8) {
+                System.out.println(arrayen[j]);
             }
         }
     }
