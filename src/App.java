e36b1ecb5ad2d019c5905da7b9ab20e56da8997d
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2024-03-13 10:30:48 +0100

    skrev ut fältets innehåll genom for-loop

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] fält = {1,2,2,15,7,9,6,3,8,4};
+        for (int i = 0; i < fält.length; i++) {
+            System.out.println(fält[i]);
+        }
+    }
+}
